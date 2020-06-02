6faead7214adbc73ada6cabdf75bfc242af9d767
YASWANT KUMAR SINGHI <40240984+Yaswant-Kumar-Singhi@users.noreply.github.com>
2020-06-02 20:28:02 +0530

    Create pattern02.java

---
+++ pattern2/pattern02.java
@@ -0,0 +1,17 @@
+
+public class Pattern02 
+{
+  public static void main(String[] args) 
+  {
+    int size=5; 
+    for(int i=1;i<=size;i++) 
+       {
+         for(int j=1;j<=i;j++) 
+          {
+             System.out.print(j); 
+          }
+        System.out.println(); 
+       }
+  }
+}
+
