aac39ab2bb50028db85db15169b594501cf6c181
YASWANT KUMAR SINGHI <40240984+Yaswant-Kumar-Singhi@users.noreply.github.com>
2020-06-03 20:06:45 +0530

    Update pattern02.java

--- pattern2/pattern02.java
+++ pattern2/pattern02.java
@@ -1,17 +1,24 @@
+/*pattern:
+1
+12
+123
+1234
+12345
+*/
 
 public class Pattern02 
 {
   public static void main(String[] args) 
   {
-    int size=5; 
-    for(int i=1;i<=size;i++) 
+    int size=5; //initilizing n=5 
+    for(int i=1;i<=size;i++) // outer loop
        {
-         for(int j=1;j<=i;j++) 
+         for(int j=1;j<=i;j++) // inner loop
           {
-             System.out.print(j); 
-          }
-        System.out.println(); 
-       }
-  }
-}
+             System.out.print(j); // printing o/p
+          } // end of inner loop
+        System.out.println(); // changing line
+       }// end of outer loop
+  }// end of main class
+}// end of program
 
