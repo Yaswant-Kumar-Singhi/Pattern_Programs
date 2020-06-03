b26d35893dc5f862afcd662fe56aee838ac1a223
YASWANT KUMAR SINGHI <40240984+Yaswant-Kumar-Singhi@users.noreply.github.com>
2020-06-03 20:08:53 +0530

    Update pattern02.cpp

--- pattern2/pattern02.cpp
+++ pattern2/pattern02.cpp
@@ -1,14 +1,23 @@
+/*Pattern:
+1
+12
+123
+1234
+12345
+*/
 
+//including header files
 #include<bits/stdc++.h>
 using namespace std;
 
+//main method
 int  main()
 {
- for(int i=1;i<=5;i++)
+ for(int i=1;i<=5;i++) // outer loop for rows
  {
-  for(int j=1;j<=i;j++)
-     cout<<j;
+  for(int j=1;j<=i;j++) // inner loop for columns
+     cout<<j; // printing
  }
-  cout<<endl;
+  cout<<endl; //changing lines
   return 0;
-}
+}//end of program
